f5ba13cbec50f61e74cecd80c97240fd6a851db7
Nandini Khandelwal <nandini.khndlwl16@gmail.com>
2026-03-24 05:45:23 +0530

    Collaborative Space Filter

--- Habit Building App/cellls/AddHabitsViewCells/CollaborativeActivityListCell.swift
+++ Habit Building App/cellls/AddHabitsViewCells/CollaborativeActivityListCell.swift
@@ -11,6 +11,9 @@ class CollaborativeActivityListCell: UICollectionViewCell {
 
     @IBOutlet weak var activityImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var materialsLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -28,12 +31,16 @@ class CollaborativeActivityListCell: UICollectionViewCell {
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowOpacity = 0.15
         layer.shadowOffset = CGSize(width: 0, height: 4)
-        layer.shadowRadius = 8
+        layer.shadowRadius = 6
         layer.masksToBounds = false
     }
 
     func configureCell(with activity: CollaborativeActivityList) {
         titleLabel.text = activity.title
         activityImage.image = UIImage(named: activity.activityImage)
+
+        timeLabel.text = "⏱ \(activity.timeLabel)"
+        ageLabel.text = "👶 \(activity.ageRange)"
+        materialsLabel.text = "🎒 \(activity.materialsLabel)"
     }
 }
---
+++ Habit Building App/controllers/subordinate VC/SectionHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  SectionHeaderView.swift
+//  Habit Building App
+//
+//  Created by Nandini Khandelwal on 24/03/26.
+//
+
+import UIKit
+
+class SectionHeaderView: UICollectionReusableView {
+        
+}
